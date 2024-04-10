95dab70cff9bf6e4874cdff600bc57bebba7013a
AlianBlank <wangfj11@foxmail.com>
2024-04-10 10:43:31 +0800

    [增加]1. 增加Download组件

---
+++ Runtime/GameFrameXDownloadCroppingHelper.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace GameFrameX.Download.Runtime
+{
+    public class GameFrameXDownloadCroppingHelper : MonoBehaviour
+    {
+        private void Start()
+        {
+            _ = typeof(UnityWebRequestDownloadAgentHelper);
+            _ = typeof(Constant);
+            _ = typeof(DownloadAgentHelperCompleteEventArgs);
+            _ = typeof(DownloadAgentHelperErrorEventArgs);
+            _ = typeof(DownloadAgentHelperUpdateBytesEventArgs);
+            _ = typeof(DownloadAgentHelperUpdateLengthEventArgs);
+            _ = typeof(DownloadFailureEventArgs);
+            _ = typeof(DownloadManager);
+            _ = typeof(DownloadStartEventArgs);
+            _ = typeof(DownloadSuccessEventArgs);
+            _ = typeof(DownloadUpdateEventArgs);
+            _ = typeof(IDownloadAgentHelper);
+            _ = typeof(IDownloadManager);
+            _ = typeof(DownloadAgentHelperBase);
+            _ = typeof(DownloadComponent);
+        }
+    }
+}
